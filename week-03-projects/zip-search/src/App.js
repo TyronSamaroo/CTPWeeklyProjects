1e706a1ccf16211f8306fb7970b9db8198155b5e
Tyron Samaroo <tyronsamaroo@gmail.com>
2019-09-20 12:54:26 -0400

    Update Week 3 Project ZipCode Search

--- week-03-projects/zip-search/src/App.js
+++ week-03-projects/zip-search/src/App.js
@@ -8,18 +8,49 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Try 11420"
+        onChange={props.onChange}
+       />
+    </div>
+    );
 }
 
 
+
+
 class App extends Component {
+  // function
+  onZipSearchFieldChange = (event) => {
+   // console.log("hi");
+   const zipcode = event.target.value;
+   // http://ctp-zip-api.herokuapp.com/zip/:zipcode
+   fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipcode}`)
+    .then( response => {
+      console.log("Response")
+      if(response.ok){
+        return response.json();
+      }
+    })
+    .then(cities =>{
+      console.log(cities);
+    })
+    .catch(error => {
+      console.log('Error:' , error);
+    })
+  }
+  // Can change line 37 to {event {this.zip}}
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        {/* <ZipSearchField onChange={this.onZipSearchFieldChange}/>   */}
+        <ZipSearchField onChange={ (event) => this.onZipSearchFieldChange(event)}/> 
         <div>
           <City />
           <City />
